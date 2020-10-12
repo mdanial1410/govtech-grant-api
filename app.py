69fea29841f2416e18843503407c3261f2656a01
Muhd Danial <mdanial1410@outlook.com>
2020-10-12 15:27:28 +0800

    added endpoints and modified the code for readability

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, jsonify, request, make_response
 from flask_mongoengine import MongoEngine
-
+from mongoengine import DoesNotExist
 
 app = Flask(__name__)
 
@@ -30,17 +30,12 @@ class Family(db.EmbeddedDocument):
     name = db.StringField()
     # gender = db.StringField()
 
+
 class House(db.Document):
     house_id = db.IntField(required=True, unique=True)
     housingType = db.StringField(required=True)
     family = db.EmbeddedDocumentListField(Family)
 
-    # def to_json(self):
-    #     return {
-    #         "house_id": self.house_id,
-    #         "housingType": self.housingType,
-    #         "family_members": self.family
-    #     }
 
 @app.route('/api/create_household', methods=['POST'])
 def create_household():
@@ -55,7 +50,7 @@ def create_household():
         h = House(house_id=content['house_id'], housingType=content['housingType'])
         h.save()
         h_id.append(h.house_id)
-    return make_response("House ID: {} successfully created.".format(h_id), 201)
+    return make_response(f"House ID: {h_id} successfully created.", 201)
 
 
 @app.route('/api/add_family/<h_id>', methods=['POST'])
@@ -63,21 +58,41 @@ def add_family(h_id):
     content = request.json
     # House.objects.get(house_id=h_id).update(**content)
     h = House.objects.get(house_id=h_id)
-    print(h.to_json())
-    return make_response("Added family member successfully", 201)
+    print(h['family'])
+    # print(h.to_json())
+    # h.family.append(content['family'])
+    # h.save()
+    return make_response(f"Added family member in House ID:{h_id} successfully", 201)
+
 
 @app.route('/api/list_household', methods=['GET'])
 def list_household():
     houses = House.objects()
     return make_response(jsonify(houses), 200)
 
+
 @app.route('/api/show_household/<h_id>', methods=['GET'])
 def show_household(h_id):
-    houses = []
-    for house in House.objects:
-        houses.append(house)
-    return make_response(jsonify(houses), 200)
+    try:
+        house = House.objects.get(house_id=h_id)
+    except DoesNotExist:
+        return make_response(f'House ID: {h_id} does not exist!', 400)
+    return make_response(jsonify(house), 200)
 
+
+@app.route('/api/del_household/<h_id>', methods=['DELETE'])
+def del_household(h_id):
+    House.objects.get(house_id=h_id).delete()
+    return make_response(f'House ID: {h_id} deleted successfully', 200)
+
+
+@app.route('/api/del_member/<h_id>/<fam_name>', methods=['DELETE'])
+def del_member(h_id, fam_name):
+    # House.objects.get().delete()
+    h = House.objects.get(house_id=h_id)
+    t = h.family.get(name=fam_name)
+    print(t)
+    return make_response(jsonify(t), 200)
 
 if __name__ == "__main__":
     app.run(debug=True)
